f26b2b462b63ac6c3c9a587b604d025d0b3c1977
rupalimarwadi <rupali0103@gmail.com>
2020-12-02 20:00:40 +0530

    Animation fade and slide demo

---
+++ src/app/animations.ts
@@ -0,0 +1,35 @@
+import { style, trigger, transition, animate, state } from '@angular/animations';
+export let fade = trigger('fade', [
+    // Cleaner way
+    state('void', style({ opacity: 0 })),
+    transition('void <=> *', [
+        animate(2000)
+    ]),
+    // void => * alias :enter
+    // * => void alias :leave
+    // 1st way
+    // state('void', style({ opacity: 0 })),
+    // transition('void => *', [
+    //   animate(2000)
+    // ]),
+    // transition('* => void', [
+    //   animate(2000)
+    // ])
+
+    // 2nd way
+    // transition('void => *', [
+    //   style({ opacity: 0 }), animate(2000)
+    // ]),
+    // transition('* => void', [
+    //   animate(2000, style({ opacity: 0 }))
+    // ])
+]);
+
+export let slide = trigger('slide', [
+    transition(':enter', [
+        style({ transform: 'translateX(30px)' }), animate(500)
+    ]),
+    transition(':leave', [
+        animate(500, style({ transform: 'translateX(-100px)' })),
+    ]),
+])
