fba5f84d007a66d4101c0f779076f56bcd494a23
rupalimarwadi <rupali0103@gmail.com>
2020-12-02 21:45:10 +0530

    Animate child elements with query

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
-import { style, trigger, transition, animate, state, keyframes, animation, useAnimation, } from '@angular/animations';
+import { style, trigger, transition, animate, state, keyframes, animation, useAnimation, query, animateChild } from '@angular/animations';
 import { bounceOutLeftAnimation, fade, slide, fadeInAnimation } from 'app/animations';
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
-  animations: [trigger('todoAnimation',
-    [
+  animations: [
+    trigger('todosAnimation', [
       transition(':enter',
-        [useAnimation(fadeInAnimation, {
-          params: {
-            duration: '500ms',
-            easing: 'ease-out'
-          }
-        })]
-      ),
-      transition(':leave',
-        [style({ backgroundColor: 'red' }), animate(1000), useAnimation(bounceOutLeftAnimation)]),
-    ]
-  )]
+        [
+          query('h1',
+            [style({ transform: 'translateY(-20px)', opacity: 1 }), animate(1000)]),
+          query('@todoAnimation', animateChild()),
+        ])
+    ]),
+    trigger('todoAnimation',
+      [
+        transition(':enter',
+          [useAnimation(fadeInAnimation, {
+            params: {
+              duration: '500ms',
+              easing: 'ease-out'
+            }
+          })]
+        ),
+        transition(':leave',
+          [style({ backgroundColor: 'red' }), animate(1000), useAnimation(bounceOutLeftAnimation)]),
+      ]
+    )]
   // animations: [slide]
   // animations: [fade]
 })
@@ -37,5 +46,11 @@ export class TodosComponent {
   removeItem(item) {
     let index = this.items.indexOf(item);
     this.items.splice(index, 1);
+  }
+  animationStart(event) {
+    console.log(event)
+  }
+  animationDone(event) {
+    console.log(event)
   }
 }
